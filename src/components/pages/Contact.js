e2d790f9bd394b34719487797974275c6b61a1cb
Jayden Cowley <jcowley16@gmail.com>
2022-07-22 07:17:42 -0600

    Adding content to the Contact page

---
+++ src/components/pages/Contact.js
@@ -0,0 +1,25 @@
+import React from "react"
+
+<div>
+            <h2>
+                Contact Me
+            </h2>
+            <h3>
+                Jayden Cowley
+            </h3>
+            <p>
+                I'd be happy to hear from you!
+            </p>
+            <address>
+                3697 West 4150 South<br />
+                West Haven UT<br />
+                84401<br />
+                <br />
+                P: (801)675-6405 <br />
+                E: <a href="mailto:jcowley16@gmail.com">Send Me An email</a><br />
+                <a href="https://github.com/JaydenCowley" target="_blank">Visit my Github</a> <br />
+                <a href="https://www.linkedin.com/in/jayden-cowley-5b5429230/" target="_blank">linkedin</a>
+            </address>
+
+
+        </div>
