6ec5c732599c80ef61efd321f0366de293b27a9e
Jayden Cowley <jcowley16@gmail.com>
2022-07-22 07:11:07 -0600

    Adding content to the About page

---
+++ src/components/pages/About.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function About() {
+    return (
+        <div>
+            <h2>
+                About Me
+            </h2>
+
+            <p>
+
+                My name is Jayden Cowley and this is my portfolio! In this section I'd like to tell you a little about
+                me.
+                Some of my favorite hobbies are camping, playing video games, and spending time with my family. I have a
+                wife
+                and a baby boy. I also love dogs, I've never known a time in my life where I didn't have a furry
+                companion by my side.
+                Currently I have a Pitbull/lab mix who is about 5 years old.
+            </p>
+            <img src="" alt="Jayden and his son Lincoln"/>
+        </div>
+    )
+}
