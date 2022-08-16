7b0173727be0ed34fdebdab75ddcc5d1b5ea65d6
Jayden Cowley <jcowley16@gmail.com>
2022-08-15 18:23:32 -0600

    created images folder and added images to it

--- src/components/PortCard.js
+++ src/components/PortCard.js
@@ -4,7 +4,7 @@ import React from "react";
 export default function PortCard(props) {
   // create a component to accept props to render a Portfolio card
   return (
-    <div variant="outlined" className="bubble">
+    <div className="bubble">
       <a href={props.href} target="_blank">
         {props.title}
       </a>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import NavTabs from './components/NavTabs';
 import '@picocss/pico/css/pico.min.css'
+import './App.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
